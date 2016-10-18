419f4ede6c34a625595fcb8d7a6658afc0b83575
Frank Yang <yangpoan@gmail.com>
2016-10-18 21:47:03 +0800

    add Table UserProfile

--- junyi/users/models.py
+++ junyi/users/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    email = models.EmailField(primary_key=True)
+    fb_id = models.TextField()
+    name = models.TextField()
+    link = models.URLField()
+    gender = models.TextField()
+    locale = models.TextField()
+    picture_url = models.URLField()
+    timezone = models.TextField()
+    access_token = models.TextField()
+
+    def __str__(self):
+        return "%s %s" % (str(self.pk), self.name)

09f53389ab55b559842bb5006a556bc7efc1695d
Frank Yang <yangpoan@gmail.com>
2016-10-18 21:47:44 +0800

    add api /users/facebook/login/ & /users/facebook/

--- junyi/junyi/urls.py
+++ junyi/junyi/urls.py
@@ -1,8 +1,7 @@
 """junyi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/ Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
@@ -13,9 +12,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls'))
 ]
--- junyi/users/urls.py
+++ junyi/users/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from users import views
 
 urlpatterns = [
+    url(r'^facebook/login/$', views.FBUserLogin.as_view()),
+    url(r'^facebook/$', views.FBUserRedirect.as_view()),
 ]
